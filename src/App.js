f2754cf0d9ef5965e05c3dd543d60858d00ede5f
JoniBach <james@jamescrook.uk>
2020-08-20 15:14:36 +0100

    initial commit of dashboard with side menu structure in place

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainView from './components/MainView/MainView'
+
+function App() {
+  return (
+ <div>
+   <MainView/>
+ </div>
+  );
+}
+
+export default App;
